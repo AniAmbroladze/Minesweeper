269d0d3c2fcfe9cbe8937c9f3da0915bb42e3cda
Ani Ambroladze <45206411+AniAmbroladze@users.noreply.github.com>
2023-12-22 14:47:30 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,3 @@ from board import Board
 
 game = UI()
 game.run()
-
-
-# difficulty levels
-# customized? : max 30x24/ 200mines
-# safe Startmode: YES/NO - click and do not put mines around the area
